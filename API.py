61c57092f388c0de7e53ef09d03dc1c8defdd461
Jardiel <Jardiel.CarlosM@hotmail.com>
2022-03-30 10:20:15 -0300

    Update API.py

--- API.py
+++ API.py
@@ -1,24 +1,31 @@
 from flask import Flask, request
 
 filmes = [
-    {'nome':'Harry Potter', 'descricao':'filme sobre uma escola de bruxaria', 'id': "0"},
-    {'nome':'Velozes e Furiosos', 'descricao':'Filme de acao com corrida ', 'id': "1"},  #Sem caracteres especiais para não bugar o browser
-    {'nome':'Homem-Aranha', 'descricao':'Historia de um super heroi', 'id': "2"}
+    {'nome': 'Harry Potter', 'descricao': 'filme sobre uma escola de bruxaria', 'id': "0"},
+    {'nome': 'Velozes e Furiosos', 'descricao': 'Filme de acao com corrida ',
+        'id': "1"},  # Sem caracteres especiais para não bugar o browser
+    {'nome': 'Homem-Aranha', 'descricao': 'Historia de um super heroi', 'id': "2"}
 ]
 
 app = Flask(__name__)
 
-#Página inicil.
+# Página inicil.
+
+
 @app.route("/")
 def home():
     return "<h1>Página Inicial de filmes</h1>"
 
-    #Mostra a lista de filmes.
+    # Mostra a lista de filmes.
+
+
 @app.route("/filmes", methods=["GET"])
 def get():
     return {'filmes': filmes}
 
-#Coloca um novo filme na lista de filmes.
+# Coloca um novo filme na lista de filmes.
+
+
 @app.route("/novo_filme", methods=["POST"])
 def post():
     body = request.json
@@ -34,16 +41,18 @@ def post():
     if ("nome" not in body):
         return {"status": 400, "mensagem": "O paramentro nome é obrigatorio"}
 
-    filmes.insert(tamanho + 1, {"id": f'{body["id"]}', "nome": f'{body["nome"]}', 'descricao': f'{body["descricao"]}'})
+    filmes.insert(
+        tamanho + 1, {"id": f'{body["id"]}', "nome": f'{body["nome"]}', 'descricao': f'{body["descricao"]}'})
 
     return {'filmes': filmes}
 
+    # Atualizar algum filme
 
-        #Atualizar algum filme
+
 @app.route("/atualizar_filme", methods=["PATCH"])
 def path():
     body = request.json
-    int(body["id"])                      #ATENÇÃO NECESSARIO USAR O ID PARA ENCONTRAR O FILME
+    int(body["id"])  # ATENÇÃO NECESSARIO USAR O ID PARA ENCONTRAR O FILME
     try:
         filmes[int(body["id"])]["nome"] = body["nome"]
 
@@ -56,8 +65,9 @@ def path():
 
     return {'filmes': filmes}
 
+    # Deletar filme
 
-        #Deletar filme
+
 @app.route("/deletar_filme", methods=["DELETE"])
 def delete():
     body = request.json
